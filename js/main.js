70e322ab66575690cb78417951a8a71db6df7724
zamao9 <mansory_09@mail.ru>
2024-10-07 02:40:04 +0300

    added home section, and media for header/home, added function on scroll for burger and navigation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const header = document.querySelector('.header');
 	const burger = document.querySelector('.burger');
 	const navigation = document.querySelector('.navigation');
 	const body = document.getElementsByTagName('body');
+
+	/* Header On Scroll */
+	window.onscroll = () => {
+		let scrollY = window.scrollY;
+
+		if (scrollY > 0) {
+			if (window.innerWidth >= 768) {
+				header.classList.add('is-active');
+			}
+			burger.style.background = '#404040';
+			burger.style.border = '1px solid #595959';
+			burger.style.borderRight = '0';
+		} else {
+			if (window.innerWidth >= 768) {
+				header.classList.remove('is-active');
+			}
+			burger.style.background = 'transparent';
+			burger.style.borderRadius = '';
+			burger.style.border = '0';
+		}
+	};
 
 	/* Burger On Click */
 	burger.addEventListener('click', (element) => {
 		element.currentTarget.classList.toggle('is-active');
 		/* Navigation Is Active*/
 		navigation.classList.toggle('is-active');
+		if (navigation.classList.contains('is-active')) {
+			burger.style.background = 'transparent';
+			burger.style.borderRadius = '';
+			burger.style.border = '0';
+		} else if (navigation && window.scrollY > 0) {
+			burger.style.background = '#404040';
+			burger.style.border = '1px solid #595959';
+			burger.style.borderRight = '0';
+		}
 		/* Body Overflow Hidden */
 		body[0].classList.toggle('is-block');
 	});
